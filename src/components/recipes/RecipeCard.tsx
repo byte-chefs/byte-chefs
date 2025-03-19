05d7b828c592385b290a450d433c8c3eb24d6961
Daria Pronina <daria.pro@rockalab.com>
2025-03-19 21:31:50 +0200

    revert deleted fix

--- src/components/recipes/RecipeCard.tsx
+++ src/components/recipes/RecipeCard.tsx
@@ -6,7 +6,7 @@ import { Clock } from 'lucide-react'
 
 const RecipeCard: FC<RecipeCardProps> = ({ recipe }) => {
   return (
-    <div className="w-full">
+    <Link href={`/recipe/${recipe?.id}`} className="w-full">
       <div className="relative aspect-[10/13] w-full overflow-hidden rounded-xl shadow-2xl md:rounded-3xl">
         <Image
           src={recipe?.photo || '/recipe-placeholder.png'}
@@ -16,15 +16,13 @@ const RecipeCard: FC<RecipeCardProps> = ({ recipe }) => {
         />
       </div>
       <div className="mt-2 flex flex-col md:mt-6">
-        <Link href={`/recipe/${recipe?.id}`} className="inline-block">
-          <h3 className="leading-none md:font-bold">{recipe?.name}</h3>
-        </Link>
+        <h3 className="leading-none md:font-bold">{recipe?.name}</h3>
         <div className="mt-2 flex items-center gap-1 md:mt-4 md:gap-3">
           <Clock className="h-3 w-3 md:h-6 md:w-6" />
           <p className="leading-none">{recipe?.cookingTime} min</p>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
